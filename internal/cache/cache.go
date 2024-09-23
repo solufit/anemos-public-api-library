fe8f59382afaf92faef33f230ed7272add81055c
Yuta Takahashi <y.highbridge04@gmail.com>
2024-09-23 14:55:52 +0000

    Create cache based on API data

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -4,14 +4,8 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// Anemosにおいて、Cacheを扱うための構造体
-type AnemosCache struct {
-	redisClient *redis.Client
-}
+// APIから取得した情報をもとに、Cacheを作成する
+func CreateCache(redisClient *redis.Client) error {
 
-// AnemosCacheのコンストラクタ
-func NewAnemosCache(redisOption *redis.Options) *AnemosCache {
-
-	redisClient := redis.NewClient(redisOption)
-	return &AnemosCache{redisClient: redisClient}
+	return nil
 }
