987d532ad06870f8b4f737c24eee0c5f66d61d97
Yuta Takahashi <y.highbridge04@gmail.com>
2024-09-23 15:27:20 +0000

    Refactor cache creation logic and optimize data storage
    This commit refactors the CreateCache function in the cache.go file. It introduces two new types, eventType and id, to represent event types and event IDs respectively. The function now takes an additional parameter, anemosData, which is used to create the cache based on the data retrieved from the API.
    
    The changes include:
    - Creating a map to store the data to be saved in Redis, using the event ID as the key
    - Creating a map to store the event IDs for each event type for the past week
    - Creating a list of event types
    - Populating the cache data and the weekly event data based on the anemosData
    
    These changes optimize the storage and retrieval of data in the cache.

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -4,8 +4,35 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+// Anemosイベントタイプ
+type eventType string
+
+// AnemosイベントID
+type id string
+
 // APIから取得した情報をもとに、Cacheを作成する
-func CreateCache(redisClient *redis.Client) error {
+func CreateCache(redisClient *redis.Client, anemosData []interface{}) error {
+
+	// Redisに保存するデータ
+	var target_data = make(map[id]string)
+
+	// 一週間分のキャッシュデータインデックスを作成する
+	var weekly_data = make(map[eventType][]id)
+
+	// Object Typeの一覧
+	var object_types []eventType
+
+	// 一週間分のキャッシュデータを作成する
+	for _, data := range anemosData {
+		var object_id id = data.(map[string]interface{})["info_objectId"].(id)
+		var object_type eventType = data.(map[string]interface{})["object_type"].(eventType)
+		//objectidをキーにして、データを保存する
+		target_data[object_id] = data.(string)
+
+		//イベントタイプをキーにして、objectidを保存する
+		weekly_data[object_type] = append(weekly_data[object_type], object_id)
+
+	}
 
 	return nil
 }
