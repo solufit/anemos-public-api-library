39c6e040359cb50a1a4e765ab1afaa44911a8791
Yuta Takahashi <y.highbridge04@gmail.com>
2024-10-01 15:13:09 +0000

    Refactor package name and add CreateCache function

--- libanemos.go
+++ libanemos.go
@@ -1,13 +1,19 @@
+// Package libanemos provides structures and functions to work with Anemos data.
 package libanemos
 
+// AnemosGet is a structure that holds a slice of anemosData of any type.
 type AnemosGet struct {
 	Data []anemosData[any]
 }
 
+// The anemosData interface defines a generic type T and requires a Filter method
+// that returns an anemosData of the same type.
 type anemosData[T any] interface {
 	Filter() anemosData[T]
 }
 
+// NewAnemosGet initializes and returns a pointer to an AnemosGet instance with an
+// empty slice of anemosData.
 func NewAnemosGet() *AnemosGet {
 	anemosget := AnemosGet{
 		Data: []anemosData[any]{},
